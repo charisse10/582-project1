7ae7d77424c9abebdf164d13034b5ae7a601b5ba
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-07 19:19:27 -0400

    adding more tests

--- tests/unit/FavouritesList.spec.js
+++ tests/unit/FavouritesList.spec.js
@@ -2,11 +2,11 @@ import { shallowMount } from "@vue/test-utils";
 import ProjectItem from "@/components/FavouritesList.vue";
 
 describe("FavouritesList.vue", () => {
-  it("emits liked event when action is made in ProejctItem", () => {
+  it("emits liked event when action is made in ProjectItem", () => {
 
   });
 
-  it("emits unliked event when action is made in ProejctItem", () => {
+  it("emits unliked event when action is made in ProjectItem", () => {
     
   });
 
--- tests/unit/ProjectItem.spec.js
+++ tests/unit/ProjectItem.spec.js
@@ -2,10 +2,12 @@ import { shallowMount } from "@vue/test-utils";
 import ProjectItem from "@/components/ProjectItem.vue";
 
 describe("ProjectItem.vue", () => {
+
   it("renders props: project when passed", () => {
 
   });
 
+  // BUTTON
   it("emits like event when like button is clicked", () => {
 
   });
@@ -14,14 +16,35 @@ describe("ProjectItem.vue", () => {
 
   });
 
-  it("displays comment section when liked is emmited", () => {
+  it("emits add to favourites event when add to favourites button is clicked", () => {
+
+  });
+
+  it("emits remove from favourites event when remove from favourites button is clicked", () => {
 
   });
 
   it("emits unlike event when like button is clicked", () => {
 
   });
+  
 
+// COMMENT
 
+  it("renders comment correctly", () => {
+
+  });
+
+  it("adds a comment when submit comment button is clicked", () => {
+
+  });
+
+  it("not add a comment when comment is empty", () => {
+
+  });
+  
+  it ("clears textarea when submit comment button is clicked", () => {
+
+    });
 
 });
