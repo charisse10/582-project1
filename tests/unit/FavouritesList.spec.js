5348fa0bb08bd74bbecaf51a17a01b88a19ef360
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-07 18:27:41 -0400

    adding tests for FavouritesList

---
+++ tests/unit/FavouritesList.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
+import ProjectItem from "@/components/FavouritesList.vue";
+
+describe("FavouritesList.vue", () => {
+  it("emits liked event when action is made in ProejctItem", () => {
+
+  });
+
+  it("emits unliked event when action is made in ProejctItem", () => {
+    
+  });
+
+});
