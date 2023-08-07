0ce8f322ac32d53b93bf3b74df968cc0b4a6029e
charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-07 18:09:37 -0400

    adding tests to ProjectItem

---
+++ tests/unit/ProjectItem.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from "@vue/test-utils";
+import ProjectItem from "@/components/ProjectItem.vue";
+
+describe("ProjectItem.vue", () => {
+  it("renders props: project when passed", () => {
+
+  });
+
+  it("emits like event when like button is clicked", () => {
+
+  });
+
+  it("displays class when liked is emmited", () => {
+
+  });
+
+  it("displays comment section when liked is emmited", () => {
+
+  });
+
+  it("emits unlike event when like button is clicked", () => {
+
+  });
+
+
+
+});
