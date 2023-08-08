512490f56af433bd700015990506d17b2e70d669
Charisse10 <1995510@edu.vaniercollege.qc.ca>
2023-08-08 10:28:47 -0400

    opening file

--- tests/unit/FavouritesList.spec.js
+++ tests/unit/FavouritesList.spec.js
@@ -2,12 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import ProjectItem from "@/components/FavouritesList.vue";
 
 describe("FavouritesList.vue", () => {
-  it("emits liked event when action is made in ProjectItem", () => {
+  it("emits liked event when action is made in ProjectItem", () => {});
 
-  });
-
-  it("emits unliked event when action is made in ProjectItem", () => {
-    
-  });
-
+  it("emits unliked event when action is made in ProjectItem", () => {});
 });
--- tests/unit/ProjectItem.spec.js
+++ tests/unit/ProjectItem.spec.js
@@ -2,49 +2,26 @@ import { shallowMount } from "@vue/test-utils";
 import ProjectItem from "@/components/ProjectItem.vue";
 
 describe("ProjectItem.vue", () => {
-
-  it("renders props: project when passed", () => {
-
-  });
+  it("renders props: project when passed", () => {});
 
   // BUTTON
-  it("emits like event when like button is clicked", () => {
+  it("emits like event when like button is clicked", () => {});
 
-  });
+  it("displays class when liked is emmited", () => {});
 
-  it("displays class when liked is emmited", () => {
+  it("emits add to favourites event when add to favourites button is clicked", () => {});
 
-  });
+  it("emits remove from favourites event when remove from favourites button is clicked", () => {});
 
-  it("emits add to favourites event when add to favourites button is clicked", () => {
+  it("emits unlike event when like button is clicked", () => {});
 
-  });
+  // COMMENT
 
-  it("emits remove from favourites event when remove from favourites button is clicked", () => {
+  it("renders comment correctly", () => {});
 
-  });
+  it("adds a comment when submit comment button is clicked", () => {});
 
-  it("emits unlike event when like button is clicked", () => {
+  it("not add a comment when comment is empty", () => {});
 
-  });
-  
-
-// COMMENT
-
-  it("renders comment correctly", () => {
-
-  });
-
-  it("adds a comment when submit comment button is clicked", () => {
-
-  });
-
-  it("not add a comment when comment is empty", () => {
-
-  });
-  
-  it ("clears textarea when submit comment button is clicked", () => {
-
-    });
-
+  it("clears textarea when submit comment button is clicked", () => {});
 });
